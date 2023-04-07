8a7d6f7d412df4094973c12d3e3b8a903a62c8f8
Christian Gröling <contact@christiangroeling.de>
2023-04-07 10:09:19 +0200

    Add: Drawing Example with Skia

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,8 +1,25 @@
-﻿internal class Program
+﻿using SkiaSharp;
+
+internal class Program
 {
   private static void Main(string[] args)
   {
     Console.WriteLine("Hello, World!");
+
+
+    // Create an image and fill it blue
+    SKBitmap bmp = new(640, 480);
+    using SKCanvas canvas = new(bmp);
+    canvas.Clear(SKColor.Parse("#003366"));
+
+    // Draw lines with random positions and thicknesses
+    Random rand = new(0);
     var classobj = new darkside_div.Class1();
+    classobj.Draw(canvas, bmp.Width, bmp.Height);
+
+    // Save the image to disk
+    SKFileWStream fs = new("quickstart.jpg");
+    bmp.Encode(fs, SKEncodedImageFormat.Jpeg, quality: 85);
+
   }
 }
--- darkside_div/Class1.cs
+++ darkside_div/Class1.cs
@@ -1,5 +1,20 @@
-﻿namespace darkside_div;
+﻿using SkiaSharp;
+
+namespace darkside_div;
+
 public class Class1
 {
+  public void Draw(SKCanvas canvas, int width, int height)
+  {
+    Random rand = new(0);
+    SKPaint paint = new() { Color = SKColors.White.WithAlpha(100), IsAntialias = true };
+    for (int i = 0; i < 100; i++)
+    {
+      SKPoint pt1 = new(rand.Next(width), rand.Next(height));
+      SKPoint pt2 = new(rand.Next(width), rand.Next(height));
+      paint.StrokeWidth = rand.Next(1, 10);
+      canvas.DrawLine(pt1, pt2, paint);
+    }
 
+  }
 }
