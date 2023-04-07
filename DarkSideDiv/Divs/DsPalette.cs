a2f565689f70228894bed04e5f39879840e50f28
Christian Gröling <contact@christiangroeling.de>
2023-04-07 10:09:20 +0200

    Refactoring

--- DarkSideDiv/Divs/DsPalette.cs
+++ DarkSideDiv/Divs/DsPalette.cs
@@ -6,44 +6,46 @@ namespace DarkSideDiv.Divs
   {
     public SKColor GetColorByIdx(int idx)
     {
-      var fill_color = SKColor.Parse("#FFFFFF");
+      var color = SKColor.Parse("#FFFFFF");
       switch (idx)
       {
         case 0:
-          fill_color = SKColor.Parse("#dbd4d0");
+          color = SKColor.Parse("#dbd4d0");
           break;
         case 1:
-          fill_color = SKColor.Parse("#ded9c1");
+          color = SKColor.Parse("#ded9c1");
           break;
         case 2:
-          fill_color = SKColor.Parse("#c3d9f4");
+          color = SKColor.Parse("#c3d9f4");
           break;
         case 3:
-          fill_color = SKColor.Parse("#dae5f2");
+          color = SKColor.Parse("#dae5f2");
           break;
         case 4:
-          fill_color = SKColor.Parse("#fffeff");
+          color = SKColor.Parse("#fffeff");
           break;
         case 5:
-          fill_color = SKColor.Parse("#f4dbd9");
+          color = SKColor.Parse("#f4dbd9");
           break;
         case 6:
-          fill_color = SKColor.Parse("#eaf1dd");
+          color = SKColor.Parse("#eaf1dd");
           break;
         case 7:
-          fill_color = SKColor.Parse("#e5dfed");
+          color = SKColor.Parse("#e5dfed");
           break;
         case 8:
-          fill_color = SKColor.Parse("#d7eef3");
+          color = SKColor.Parse("#d7eef3");
           break;
 
 
         default:
-          fill_color = SKColor.Parse("#FFFFFF");
+          color = SKColor.Parse("#FFFFFF");
           break;
       }
-      return fill_color;
+      return color;
     }
+
+    
   }
 
 }
