7bdf0f95a09e224231d6c3fba9df2ea5035adfa8
Christian Gröling <contact@christiangroeling.de>
2023-04-07 10:09:21 +0200

    Feature: GridBuilder

--- Application/Common/Builders/DsGridBuilder.cs
+++ Application/Common/Builders/DsGridBuilder.cs
@@ -9,169 +9,83 @@ using Application.Common;
 
 namespace Application.Builders
 {
-  public class DsTrackerBuilder
+  public class DsGridBuilder
   {
-    public delegate string GetRowTextDelegate(int row);
-
-    GetRowTextDelegate _get_row_text;
-
-    public delegate string GetColTextDelegate(int col);
-
-    GetRowTextDelegate _get_col_text;
-
-    public delegate ColorString GetCellColorDelegate(int idx);
-
-    GetCellColorDelegate _get_cell_color;
-
-    public ColorString GetTrackerColor(int idx)
+    struct CellSettings
     {
-      switch (idx)
-      {
-        case 0:
-          return new ColorString("#eef0f3"); // grey
-        case 1:
-          return new ColorString("#bef29b"); // light green
-        case 2:
-          return new ColorString("#4edc84"); // green
-        case 3:
-          return new ColorString("#00b454"); // dark green
-        case 4:
-          return new ColorString("#007c3c"); // darkest green
-      }
-
-      return new ColorString("#eef0f3");
+      public ColorString Color { get; set; }
     }
 
-    public ColorString GetCellColor(int idx)
+    Dictionary<(int col, int row), CellSettings> _cell_settings;
+
+    struct RowSettings
     {
-      idx = idx - GetStartIdx(); // Remove Offset
-
-      var dt = new DateTime(YEAR, 1, 1);
-      dt = dt.AddDays(idx);
-
-      if (dt == new DateTime(YEAR, 7, 1))
-      {
-        return GetTrackerColor(1);
-      }
-      if (dt == new DateTime(YEAR, 7, 2))
-      {
-        return GetTrackerColor(2);
-      }
-      if (dt == new DateTime(YEAR, 7, 3))
-      {
-        return GetTrackerColor(3);
-      }
-      if (dt == new DateTime(YEAR, 7, 4))
-      {
-        return GetTrackerColor(4);
-      }
-      return GetTrackerColor(0);
+      public string Label { get; set; }
     }
-    public DsTrackerBuilder(IDeviceRepo device_repo, SKRect pic_rect)
+
+    Dictionary<int, RowSettings> _row_settings;
+
+    struct ColSettings
+    {
+      public string Label { get; set; }
+    }
+
+    Dictionary<int, ColSettings> _col_settings;
+
+
+    public DsGridBuilder(IDeviceRepo device_repo, SKRect pic_rect, int cols, int rows)
     {
       _device_repo = device_repo;
       _pic_rect = pic_rect;
-      _get_row_text = GetRowNameOfDay;
-      _get_col_text = GetColNameOfMonth;
-      _get_cell_color = GetCellColor;
+      _rows = rows;
+      _cols = cols;
+
+      _cell_settings = new Dictionary<(int col, int row), CellSettings>();
+      _row_settings = new Dictionary<int, RowSettings>();
+      _col_settings = new Dictionary<int, ColSettings>();
     }
 
-    public int GetStartIdx()
+
+    public void SetCellColor(int col, int row, ColorString color)
     {
-      var dt = new DateTime(YEAR, 1, 1);
-      var day_of_week = (int)dt.DayOfWeek;
-
-      // Sunday is zero
-      var start_idx = (day_of_week) % 7;
-      return start_idx;
+      var cell = _cell_settings.GetValueOrDefault((col, row));
+      cell.Color = color;
+      _cell_settings.Add((col, row), cell);
     }
 
-    public int GetEndIdx()
+    public void SetRowLabel(int row, string label)
     {
-      DateTime newdate = new DateTime(YEAR + 1, 1, 1);
-      //Substract one year
-      var last_day = newdate.AddDays(-1);
-      return last_day.DayOfYear + GetStartIdx();
+      var row_set = _row_settings.GetValueOrDefault(row);
+      row_set.Label = label;
+      _row_settings.Add(row, row_set);
     }
 
-    public string GetRowNameOfDay(int row)
+
+    public void SetColLabel(int row, string label)
     {
-      switch (row)
-      {
-        case 1:
-          return "Mon";
-        case 3:
-          return "Wed";
-        case 5:
-          return "Fri";
-      }
-      return "";
+      var col_set = _col_settings.GetValueOrDefault(row);
+      col_set.Label = label;
+      _col_settings.Add(row, col_set);
     }
 
-    public string GetMonth(int month)
-    {
-      switch (month)
-      {
-        case 1:
-          return "Jan";
-        case 2:
-          return "Feb";
-        case 3:
-          return "Mar";
-        case 4:
-          return "Apr";
-        case 5:
-          return "May";
-        case 6:
-          return "Jun";
-        case 7:
-          return "Jul";
-        case 8:
-          return "Aug";
-        case 9:
-          return "Sep";
-        case 10:
-          return "Oct";
-        case 11:
-          return "Nov";
-        case 12:
-          return "Dec";
-
-      }
-      return "";
-    }
-    public string GetColNameOfMonth(int col)
-    {
-      if (col <= 1)
-      {
-        if (col == 0)
-        {
-          return GetMonth(col + 1);
-        }
-        return "";
-      }
-      var col_startidx_this_week = col * DAYS - GetStartIdx();
-      var col_startidx_last_week = (col - 1) * DAYS - GetStartIdx();
-
-      var start_date_this_week = new DateTime(YEAR, 1, 1).AddDays(col_startidx_this_week);
-      var start_date_last_week = new DateTime(YEAR, 1, 1).AddDays(col_startidx_last_week);
-
-      // check if this week is the first "full" week in the month
-      if (start_date_this_week.Month != start_date_last_week.Month) // first week which is completly in month
-      {
-        return GetMonth(start_date_this_week.Month);
-      }
-      return "";
-    }
 
     public DsDiv CreateCell(int grid_idx)
     {
+      var col = grid_idx / ((int)_rows);
+      var row = grid_idx % ((int)_rows);
+
+      var color = new ColorString("#000000");
+      if (_cell_settings.ContainsKey((col, row)))
+      {
+        color = _cell_settings.GetValueOrDefault((col, row)).Color;
+      }
+
       var attribs = new DsDivAttribs()
       {
         Border = 0f, // outer border
         Margin = 5f,
         // Padding = 5f,
-        ContentFillColor = _get_cell_color(grid_idx),
+        ContentFillColor = color,
       };
 
       var div = new DsDiv(_device_repo.DivDevice, attribs);
@@ -180,25 +94,27 @@ namespace Application.Builders
 
     public DsDiv CreateHeaderRowCell(int col)
     {
-      var color = _palette_algo.GetColorByIdx(2);
       var attribs = new DsDivAttribs()
       {
         Border = 0f, // outer border
         Margin = (0f, 0f, 0f, 20f)
-        //Padding = 5f,
-        //ContentFillColor = color,
       };
+
+      ColSettings col_set;
+      string col_label = "";
+      if (_col_settings.TryGetValue(col, out col_set))
+      {
+        col_label = col_set.Label;
+      }
 
       var text_attribs = new DsDivComponentAlignedTextAttribs()
       {
-        Text = _get_col_text(col),
+        Text = col_label,
         TextSize = 40,
         Alignment = DsAlignment.BottomLeft
       };
 
       var text_comp = new DsDivComponentAlignedText(_device_repo.DivTextDevice, text_attribs);
-
-
       var div = new DsDiv(_device_repo.DivDevice, attribs);
       div.Append(text_comp);
       return div;
@@ -206,18 +122,21 @@ namespace Application.Builders
 
     public DsDiv CreateHeaderColCell(int row)
     {
-      var color = _palette_algo.GetColorByIdx(2);
       var attribs = new DsDivAttribs()
       {
         Border = 0f, // outer border
         Padding = 5f,
-        // Padding = 5f,
-        // ContentFillColor = color,
       };
 
+      RowSettings row_set;
+      string row_label = "";
+      if (_row_settings.TryGetValue(row, out row_set))
+      {
+        row_label = row_set.Label;
+      }
       var text_attribs = new DsDivComponentAlignedTextAttribs()
       {
-        Text = _get_row_text(row),
+        Text = row_label,
         TextSize = 40,
         Alignment = DsAlignment.Left
       };
@@ -232,28 +151,19 @@ namespace Application.Builders
 
     public DsDiv CreateGrid()
     {
-      var rows = DAYS;
-      var cols = WEEKS;
       var base_grid = new DsDivComponentGrid(
-        (int)cols,
-        (int)rows
+        (int)_cols,
+        (int)_rows
       );
       base_grid.SetDivSpacing(2f);
 
-      var idx_offst = GetStartIdx();
-      var idx_end = GetEndIdx();
-
-      for (int i = 0; i < cols * rows; i++)
+      for (int i = 0; i < _cols * _rows; i++)
       {
-        var col = i / ((int)rows);
-        var row = i % ((int)rows);
+        var col = i / ((int)_rows);
+        var row = i % ((int)_rows);
 
         var cell = CreateCell(i);
-
-        if ((i >= idx_offst) && (i < idx_end))
-        {
-          base_grid.Attach(col, row, cell);
-        }
+        base_grid.Attach(col, row, cell);
       }
 
       var attribs = new DsDivAttribs()
@@ -269,8 +179,7 @@ namespace Application.Builders
 
     public DsDiv CreateHeaderRow()
     {
-      var cols = WEEKS;
-      var base_grid = new DsDivComponentGrid((int)cols, 1);
+      var base_grid = new DsDivComponentGrid((int)_cols, 1);
       base_grid.SetDivSpacing(2f);
 
       var attribs = new DsDivAttribs()
@@ -278,8 +187,7 @@ namespace Application.Builders
       };
 
       var div = new DsDiv(_device_repo.DivDevice, attribs);
-      //var content_div = CreateContentDiv();
-      for (int i = 0; i < cols; i++)
+      for (int i = 0; i < _cols; i++)
       {
         var title_div = CreateHeaderRowCell(i);
         base_grid.Attach(i, 0, title_div);
@@ -309,8 +217,7 @@ namespace Application.Builders
 
     public DsDiv CreateHeaderCol()
     {
-      var rows = DAYS;
-      var base_grid = new DsDivComponentGrid(1, (int)rows);
+      var base_grid = new DsDivComponentGrid(1, (int)_rows);
       base_grid.SetDivSpacing(2f);
 
       var attribs = new DsDivAttribs()
@@ -319,7 +226,7 @@ namespace Application.Builders
 
       var div = new DsDiv(_device_repo.DivDevice, attribs);
 
-      for (int i = 0; i < rows; i++)
+      for (int i = 0; i < _rows; i++)
       {
         var title_div = CreateHeaderColCell(i);
         base_grid.Attach(0, i, title_div);
@@ -334,19 +241,16 @@ namespace Application.Builders
       var base_grid = new DsDivComponentGrid(2, 1);
       base_grid.SetColPercFactor(0, LEFT_SPACE_IN_PERC);
 
-      var rows = DAYS;
-      var cols = WEEKS;
       var attribs = new DsDivAttribs()
       {
         Border = 1f,
-        Position = PositionType.Absolute, // place it absolute to new rect
-        //BorderColor = new ColorString("#000000"),
+        Position = PositionType.Absolute, // place it absolute to relative rect
         Height = HeightType.Zero,
         Padding = (
           (QuantityType.FixedInPixel, 0.0f),  // left
           (QuantityType.FixedInPixel, 0.0f),  // top
           (QuantityType.FixedInPixel, 0.0f),  // right
-          (QuantityType.Percent, 100f / (cols / rows) * (1f - LEFT_SPACE_IN_PERC * 0.01f))  // bottom
+          (QuantityType.Percent, 100f / (_cols / _rows) * (1f - LEFT_SPACE_IN_PERC * 0.01f))  // bottom
       )
       };
 
@@ -366,6 +270,7 @@ namespace Application.Builders
     public DsRoot Build()
     {
       var header_row_and_content = new DsDivComponentGrid(1, 2);
+
       //base_grid.SetDivSpacing(2f);
       header_row_and_content.SetRowPercFactor(0, TOP_SPACE_IN_PERC);
       //      base_grid.SetColFixedInPixel(0, 200f);
@@ -391,20 +296,16 @@ namespace Application.Builders
       return root_div;
     }
 
-
-    DsPalette _palette_algo = new DsPalette();
-
     IDeviceRepo _device_repo;
 
     private SKRect _pic_rect;
 
-    const float WEEKS = 53f;
-    const float DAYS = 7f;
-
     const float LEFT_SPACE_IN_PERC = 3.4f;
     const float TOP_SPACE_IN_PERC = 10f;
 
-    const int YEAR = 2020;
+    int _rows;
+
+    int _cols;
   }
 
 }
--- Application/UseCaseCreateTrackerDiagram.cs
+++ Application/UseCaseCreateTrackerDiagram.cs
@@ -1,11 +1,171 @@
 ﻿using SkiaSharp; // TODO: Move to Infrastructure
 using Application.Common.Interfaces;
 using Application.Device;
+using DarkSideDiv.Common;
 
 namespace Application;
 
 public class UseCaseCreateTrackerDiagram
 {
+  class Tracker
+  {
+    public Tracker(int days, int weeks, int year)
+    {
+      _days = days;
+      _weeks = weeks;
+      _year = year;
+    }
+    
+    public ColorString GetTrackerColor(int idx)
+    {
+      switch (idx)
+      {
+        case 0:
+          return new ColorString("#eef0f3"); // grey
+        case 1:
+          return new ColorString("#bef29b"); // light green
+        case 2:
+          return new ColorString("#4edc84"); // green
+        case 3:
+          return new ColorString("#00b454"); // dark green
+        case 4:
+          return new ColorString("#007c3c"); // darkest green
+      }
+
+      return new ColorString("#eef0f3");
+    }
+
+    public int GetStartIdx()
+    {
+      var dt = new DateTime(_year, 1, 1);
+      var day_of_week = (int)dt.DayOfWeek;
+
+      // Sunday is zero
+      var start_idx = (day_of_week) % 7;
+      return start_idx;
+    }
+
+    public int GetEndIdx()
+    {
+      DateTime newdate = new DateTime(_year + 1, 1, 1);
+      //Substract one year
+      var last_day = newdate.AddDays(-1);
+      return last_day.DayOfYear + GetStartIdx();
+    }
+
+
+    public ColorString GetCellColor(int idx)
+    {
+      if ((idx < GetStartIdx()) || (idx >= GetEndIdx()))
+      {
+        return new ColorString("#ffffff");
+      }
+
+      idx = idx - GetStartIdx(); // Remove Offset
+
+
+
+      var dt = new DateTime(_year, 1, 1);
+      dt = dt.AddDays(idx);
+
+      if (dt == new DateTime(_year, 7, 1))
+      {
+        return GetTrackerColor(1);
+      }
+      if (dt == new DateTime(_year, 7, 2))
+      {
+        return GetTrackerColor(2);
+      }
+      if (dt == new DateTime(_year, 7, 3))
+      {
+        return GetTrackerColor(3);
+      }
+      if (dt == new DateTime(_year, 7, 4))
+      {
+        return GetTrackerColor(4);
+      }
+
+      return GetTrackerColor(0);
+    }
+
+    public string GetRowNameOfDay(int row)
+    {
+      switch (row)
+      {
+        case 1:
+          return "Mon";
+        case 3:
+          return "Wed";
+        case 5:
+          return "Fri";
+      }
+      return "";
+    }
+
+    public string GetMonth(int month)
+    {
+      switch (month)
+      {
+        case 1:
+          return "Jan";
+        case 2:
+          return "Feb";
+        case 3:
+          return "Mar";
+        case 4:
+          return "Apr";
+        case 5:
+          return "May";
+        case 6:
+          return "Jun";
+        case 7:
+          return "Jul";
+        case 8:
+          return "Aug";
+        case 9:
+          return "Sep";
+        case 10:
+          return "Oct";
+        case 11:
+          return "Nov";
+        case 12:
+          return "Dec";
+
+      }
+      return "";
+    }
+    public string GetColNameOfMonth(int col)
+    {
+      if (col <= 1)
+      {
+        if (col == 0)
+        {
+          return GetMonth(col + 1);
+        }
+        return "";
+      }
+      var col_startidx_this_week = col * _days - GetStartIdx();
+      var col_startidx_last_week = (col - 1) * _days - GetStartIdx();
+
+      var start_date_this_week = new DateTime(_year, 1, 1).AddDays(col_startidx_this_week);
+      var start_date_last_week = new DateTime(_year, 1, 1).AddDays(col_startidx_last_week);
+
+      // check if this week is the first "full" week in the month
+      if (start_date_this_week.Month != start_date_last_week.Month) // first week which is completly in month
+      {
+        return GetMonth(start_date_this_week.Month);
+      }
+      return "";
+    }
+
+
+   int _year = 2020;
+
+    float _weeks;
+    float _days;
+
+  }
+
   private IReadTextFile _read_text_file;
 
   private IDocumentConverter _document_converter;
@@ -18,6 +178,11 @@ public class UseCaseCreateTrackerDiagram
 
   public void Execute(string input_filename, string output_filename)
   {
+    const int WEEKS = 53;
+    const int DAYS = 7;
+    // WEEKS * DAYS = 371 ... > 366 on a leap year.
+    const int YEAR = 2023;
+
     // Create an image and fill it blue
     SKBitmap bmp = new SKBitmap(3200, 1000); // 5::3
     using SKCanvas canvas = new SKCanvas(bmp);
@@ -30,19 +195,45 @@ public class UseCaseCreateTrackerDiagram
 
 
     var graph_rect = new SKRect(0.0f, 0.0f, bmp.Width, bmp.Height);
-    var lotus_builder = new Builders.DsTrackerBuilder(device_repo, graph_rect);
+    var tracker = new Tracker(DAYS, WEEKS, YEAR);
+
+    var grid_builder = new Builders.DsGridBuilder(
+      device_repo,
+      graph_rect,
+      (int)WEEKS,
+      (int)DAYS
+    );
 
     // Read Markdown File
     var text = _read_text_file.Read(input_filename);
     var topic_list = _document_converter.GetTableOfContents(text);
 
-    foreach (var i in topic_list)
+    for (int i = 0; i < (int)DAYS; i++)
     {
-      //lotus_builder.AddTopic(i.level, i.label ?? "undefined");
+      grid_builder.SetRowLabel(
+        i,
+        tracker.GetRowNameOfDay(i)
+      );
     }
-    // ------
 
-    var ds_root = lotus_builder.Build();
+    for (int i = 0; i < (int)WEEKS; i++)
+    {
+      grid_builder.SetColLabel(
+        i,
+        tracker.GetColNameOfMonth(i)
+      );
+    }
+
+    for (int i = 0; i < (int)WEEKS * (int)DAYS; i++)
+    {
+      grid_builder.SetCellColor(
+        i / (int)DAYS,
+        i % (int)(DAYS),
+        tracker.GetCellColor(i)
+      );
+    }
+
+    var ds_root = grid_builder.Build();
 
     // Draw Lotus
     ds_root.Draw();
