c0ef013876582a7e75ad6c94004887cc9e489e41
Christian Gröling <contact@christiangroeling.de>
2023-04-07 10:09:20 +0200

    Refactoring

--- DarkSideDiv/Components/GridLayout.cs
+++ DarkSideDiv/Components/GridLayout.cs
@@ -39,33 +39,29 @@ namespace DarkSideDiv.Components
 
     public IEnumerable<(int col, int row, SKRect rect)> GetRects(SKRect draw_rect)
     {
-      float row_offset = 0f;
       float col_offset = 0f;
 
       for (int col = 0; col < Columns; col++)
       {
-        row_offset = 0f;
         var col_rel = col_factors[col] / col_factors_sum;
         var width_col = draw_rect.Width * col_rel;
 
+        var left = draw_rect.Left + col_offset;
+        var right = draw_rect.Left + col_offset + width_col;
+
+        float row_offset = 0f;
         for (int row = 0; row < Rows; row++)
         {
           var row_rel = row_factors[row] / row_factors_sum;
-
-
           var height_row = draw_rect.Height * row_rel;
-
-
-          var left = draw_rect.Left + col_offset;
-          var right = draw_rect.Left + col_offset +  width_col;
 
           var top = draw_rect.Top + row_offset;
           var bottom = draw_rect.Top + row_offset + height_row;
 
           SKRect rect = new SKRect(left, top, right, bottom);
           yield return (col, row, rect);
+          
           row_offset += height_row;
-
         }
         col_offset += width_col;
       }
